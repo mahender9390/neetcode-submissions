42a21febeacff7c04d9639c866c91f3c53f4f2a0
mahender9390 <vennapureddymahender@gmail.com>
2026-05-05 20:41:49 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums)
+        {
+            set.add(i);
+        }
+        if(nums.length!=set.size())
+        {
+            return true;
+        }
+        return false;
+    }
+}
